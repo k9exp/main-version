91c1fbcf456325f0bb69f9ef66d83c5b7061f04f
Kunal Singh <kunalsin9h@gmail.com>
2025-03-12 21:04:24 +0530

    new line

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 		fmt.Println("could not read build info")
 		return
 	}
+	fmt.Println("Version using git tags!, go 1.24.1 at its peak")
 	fmt.Printf("Go Version: %s\n", info.GoVersion)
 	fmt.Printf("App Version: %s\n", info.Main.Version)
 	fmt.Printf("App Checksum: %s\n", info.Main.Sum)
